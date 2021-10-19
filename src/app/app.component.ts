242366320783357763be6453e84d71d92f3d67de
Daniel-Egorov <daniel.s.egorov@gmail.com>
2021-10-18 20:26:49 -0700

    add buttons to add/remove notes to/from an array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,15 @@
 import { Component } from '@angular/core';
 
+export class Note {
+  date: Date | undefined;
+  text: string | undefined;
+
+  constructor(date: Date, text: string) {
+    this.date = date;
+    this.text = text;
+  }
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +17,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'NotesWebApp';
+
+  public notes = Array<Note>();
+  
+  onAdd() {
+    let newNote = new Note(new Date(Date.now()), 'New Note');
+    this.notes.push(newNote);
+    console.log(this.notes);
+  }
+
+  onDelete() {
+    if (this.notes.length == 0) {
+      console.log('no notes to remove');
+      return;
+    }
+    this.notes.pop()
+    console.log(this.notes);
+  }
+  
 }
