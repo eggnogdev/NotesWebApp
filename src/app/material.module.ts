bb133c1f265305e6c62b33e7baf4c74a13f66e6c
Daniel-Egorov <daniel.s.egorov@gmail.com>
2021-10-20 17:38:07 -0700

    install material module
    implement material module for future button use,
    change styles.css to styles.scss

---
+++ src/app/material.module.ts
@@ -0,0 +1,54 @@
+import { NgModule } from '@angular/core';
+import {
+    MdcButtonModule,
+    MdcCardModule,
+    MdcCheckboxModule,
+    MdcDialogModule,
+    MdcDrawerModule,
+    MdcElevationModule,
+    MdcFabModule,
+    MdcIconModule,
+    MdcLinearProgressModule,
+    MdcListModule,
+    MdcMenuModule,
+    MdcRadioModule,
+    MdcRippleModule,
+    MdcSelectModule,
+    MdcSliderModule,
+    MdcSnackbarModule,
+    MdcSwitchModule,
+    MdcTypographyModule,
+    MdcTabBarModule,
+    MdcTopAppBarModule,
+    MdcFormFieldModule,
+    MdcTextFieldModule,
+} from '@angular-mdc/web';
+
+
+@NgModule({
+  exports: [
+    MdcButtonModule,
+    MdcCardModule,
+    MdcCheckboxModule,
+    MdcDialogModule,
+    MdcDrawerModule,
+    MdcElevationModule,
+    MdcFabModule,
+    MdcIconModule,
+    MdcLinearProgressModule,
+    MdcListModule,
+    MdcMenuModule,
+    MdcRadioModule,
+    MdcRippleModule,
+    MdcSelectModule,
+    MdcSliderModule,
+    MdcSnackbarModule,
+    MdcSwitchModule,
+    MdcTypographyModule,
+    MdcTabBarModule,
+    MdcTopAppBarModule,
+    MdcFormFieldModule,
+    MdcTextFieldModule
+  ]
+})
+export class MaterialModule {}
