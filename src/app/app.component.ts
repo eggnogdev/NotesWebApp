43da4a282be8f2b7d9c80fc705eb67fbfe7d2967
eggnogdev <github@eggnog.dev>
2021-10-26 17:25:01 -0700

    change list to angular mdc list with formatted date and on select event per list item

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,32 +2,14 @@ import { Component } from '@angular/core';
 
 export class Note {
   title: string | undefined;
-  dateObject: Date | undefined;
+  date: Date | undefined;
   text: string | undefined;
-  date: string | undefined;
 
 
-  constructor(title: string, dateObject: Date, text: string) {
-    let months = [
-      "January",
-      "February",
-      "March",
-      "April",
-      "May",
-      "June",
-      "July",
-      "August",
-      "September",
-      "October",
-      "November",
-      "December"
-    ];
+  constructor(title: string, date: Date, text: string) {
 
     this.title = title;
-    this.dateObject = dateObject;
     this.text = text;
-
-    let date = `${dateObject.getDate()} ${months[dateObject.getMonth()]} ${dateObject.getFullYear()}`;
     this.date = date;
   }
 }
@@ -39,11 +21,12 @@ export class Note {
 })
 export class AppComponent {
   title = 'NotesWebApp';
-
+  public counter = 0;
   public notes = new Array<Note>();
   
   onAdd() {
-    let newNote = new Note("New Note", new Date(Date.now()), 'text here');
+
+    let newNote = new Note(`New Note ${++this.counter}`, new Date(Date.now()), 'text here');
     this.notes.push(newNote);
     console.log(this.notes);
   }
@@ -53,10 +36,14 @@ export class AppComponent {
       console.log('no notes to remove');
       return;
     }
+    this.counter--;
     this.notes.pop()
     console.log(this.notes);
   }
   listItemClicked () {
     console.log('list item clicked');
+  }
+  onSelectionChange(event: any) {
+    console.log(event);
   }
 }
