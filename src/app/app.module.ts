43f692137fcfeae1917546c7ec19b5eccead597c
eggnogdev <github@eggnog.dev>
2021-11-01 15:05:45 -0700

    change layout and function
    make layout as wanted, notes in list next to the title and text section
    make the text fields clear when adding notes to list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import {MaterialModule} from './material.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 
 @NgModule({
@@ -11,7 +12,8 @@ import {MaterialModule} from './material.module';
   ],
   imports: [
     BrowserModule,
-    MaterialModule
+    MaterialModule,
+    FlexLayoutModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
